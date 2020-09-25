77b9c168a79da7b0cd85bcb6adcc0d46c4e8f740
Tuncay NAMLI <tuncay@srdc.com.tr>
2020-09-25 10:14:55 +0300

    :memo: docs: Progressing on example project to demonstrate how to setup a tailored FHIR server based on onFHIR for issue #14

--- onfhir-server-example/src/main/scala/io/onfhir/sample/Boot.scala
+++ onfhir-server-example/src/main/scala/io/onfhir/sample/Boot.scala
@@ -1,5 +1,11 @@
 package io.onfhir.sample
 
-object Boot {
+import io.onfhir.Onfhir
+import io.onfhir.r4.config.FhirR4Configurator
 
+object Boot extends App {
+  //Initialize onfhir for R4
+  var onfhir = Onfhir.apply(new FhirR4Configurator())
+  //Start it
+  onfhir.start
 }
