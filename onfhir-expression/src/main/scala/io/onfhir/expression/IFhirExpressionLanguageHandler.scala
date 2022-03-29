84cdf3c1b37cb29f1807a9c62c2a90dc9481c7d4
Tuncay NAMLI <tuncay@srdc.com.tr>
2022-03-29 14:38:42 +0300

    remove unnecessary method

--- onfhir-expression/src/main/scala/io/onfhir/expression/IFhirExpressionLanguageHandler.scala
+++ onfhir-expression/src/main/scala/io/onfhir/expression/IFhirExpressionLanguageHandler.scala
@@ -1,10 +1,7 @@
 package io.onfhir.expression
 
-import io.onfhir.path.{FhirPathBoolean, FhirPathComplex, FhirPathDateTime, FhirPathEvaluator, FhirPathNumber, FhirPathQuantity, FhirPathString, FhirPathTime}
 import org.json4s.{JNothing, JValue}
-import io.onfhir.util.JsonFormatter._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.matching.Regex
 
 trait IFhirExpressionLanguageHandler {
   private val internalPlaceholderExpr = """\{\{([^"{}]+)}\}""".r
@@ -46,34 +43,4 @@ trait IFhirExpressionLanguageHandler {
   @throws[FhirExpressionException]
   def satisfies(expression: FhirExpression, contextParams: Map[String, JValue], input:JValue = JNothing)(implicit ex:ExecutionContext): Future[Boolean]
 
-
-  /**
-   * Utility function to handle internal placeholders
-   * @param strValue
-   * @param fhirPathEvaluator
-   * @param input
-   * @return
-   */
-  protected def handleInternalPlaceholderMatches(strValue:String, fhirPathEvaluator:FhirPathEvaluator, input:JValue):String = {
-    def findMatch(m:Regex.Match):String = {
-      val fhirPathExpression = m.group(1)
-      val results = fhirPathEvaluator.evaluate(fhirPathExpression, input)
-      if(results.isEmpty)
-        throw FhirExpressionException(s"FHIR path expression '$fhirPathExpression' returns empty result although it is used within a FHIR search statement!")
-      (
-        results map {
-          case FhirPathComplex(_) | FhirPathQuantity(_, _) =>
-            throw FhirExpressionException(s"FHIR path expression '$fhirPathExpression' returns complex JSON object although it is used within a FHIR search statement!")
-          case FhirPathString(s) => s
-          case FhirPathNumber(n) => "" + n.toDouble.toString
-          case FhirPathBoolean(b) => "" + b
-          case dt:FhirPathDateTime => dt.toJson.toJson.dropRight(1).drop(1)
-          case t:FhirPathTime => t.toJson.toJson.dropRight(1).drop(1)
-        }
-        ).mkString(",")  // If result is multiple
-    }
-    //Fill the placeholder values
-    val filledValue = internalPlaceholderExpr.replaceAllIn(strValue, replacer = findMatch)
-    filledValue
-  }
 }
