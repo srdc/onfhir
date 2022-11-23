c905128decceb98077305749dfad611ca4ea1cbd
Tuncay NAMLI <tuncay@srdc.com.tr>
2022-11-23 14:31:09 +0300

    :sparkles: feat: Add a new method to FHIR client to easily constrcut FHIR Patch request

--- onfhir-common/src/main/scala/io/onfhir/api/client/FhirPatchRequestBuilder.scala
+++ onfhir-common/src/main/scala/io/onfhir/api/client/FhirPatchRequestBuilder.scala
@@ -1,7 +1,9 @@
 package io.onfhir.api.client
 
-import io.onfhir.api.FHIR_INTERACTIONS
+import io.onfhir.api.{FHIR_COMMON_FIELDS, FHIR_CONTENT_TYPES, FHIR_INTERACTIONS}
 import io.onfhir.api.model.FHIRRequest
+import io.onfhir.api.util.FHIRUtil
+import org.json4s.{JArray, JObject, JValue}
 
 /**
  *  Request builder for FHIR Patch interaction
@@ -14,6 +16,32 @@ class FhirPatchRequestBuilder(onFhirClient: IOnFhirClient, rtype:String, rid:Opt
     FHIRRequest(interaction = FHIR_INTERACTIONS.PATCH, requestUri = s"${onFhirClient.getBaseUrl()}/$rtype/${rid.map("/" + _).getOrElse("")}", resourceType = Some(rtype), resourceId = rid)
   ) {
   type This = FhirPatchRequestBuilder
+
+  /**
+   * Directly provide the patch content
+   * @param patch Patch content
+   *
+   *              - For JSON Patch, this can be array or object providing the patches or patch
+   *              - For FHIR Path Patch, this should be the Parameters resource
+   * @return
+   */
+  def patchContent(patch:JValue):FhirPatchRequestBuilder = {
+    patch match {
+      //If FHIR Path patch
+      case obj:JObject if obj.obj.exists(f => f._1 == FHIR_COMMON_FIELDS.RESOURCE_TYPE && FHIRUtil.extractResourceType(obj) == "Parameters") =>
+        request.contentType = Some(FHIR_CONTENT_TYPES.FHIR_JSON_CONTENT_TYPE)
+        request.resource = Some(obj)
+      //If JSON Patch with single patch
+      case obj:JObject =>
+        request.contentType = Some(FHIR_CONTENT_TYPES.FHIR_JSON_PATCH_CONTENT_TYPE)
+        request.resource = Some(JObject("patches" -> JArray(List(obj))))
+      //If JSON Patch with multiple patches
+      case arr:JArray =>
+        request.contentType = Some(FHIR_CONTENT_TYPES.FHIR_JSON_PATCH_CONTENT_TYPE)
+        request.resource = Some(JObject("patches" -> arr))
+    }
+    this
+  }
 
   def fhirPathPatch():FhirPathPatchRequestBuilder = {
     compile()
