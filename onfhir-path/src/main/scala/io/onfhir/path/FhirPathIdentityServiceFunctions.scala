32d21673c4f907a3490a0d39f854876dbb81e75f
A. Anil Sinaci <anil@srdc.com.tr>
2023-03-14 13:52:47 +0300

    :sparkles: feat: Make systemURL optional for resolveIdentifier of FhirPathIdentityServiceFunctions.

--- onfhir-path/src/main/scala/io/onfhir/path/FhirPathIdentityServiceFunctions.scala
+++ onfhir-path/src/main/scala/io/onfhir/path/FhirPathIdentityServiceFunctions.scala
@@ -7,24 +7,26 @@ import scala.concurrent.Await
 
 /**
  * Function library to access identity service functionalities
+ *
  * @param context FHIR Path context
  */
-class FhirPathIdentityServiceFunctions(context:FhirPathEnvironment) extends AbstractFhirPathFunctionLibrary {
+class FhirPathIdentityServiceFunctions(context: FhirPathEnvironment) extends AbstractFhirPathFunctionLibrary {
 
   /**
    * Resolve the given business identifier and return the FHIR Resource identifier (together with the resource type) by using the supplied identity service
-   * @param resourceTypeExpr        FHIR resource type
-   * @param identifierValueExpr     Business identifier  value (Identifier.value)
-   * @param identifierSystemExpr    Business identifier system (Identifier.system)
-   * @return                       Identifier for the resource e.g. Patient/455435464698
+   *
+   * @param resourceTypeExpr     FHIR resource type
+   * @param identifierValueExpr  Business identifier  value (Identifier.value)
+   * @param identifierSystemExpr Business identifier system (Identifier.system)
+   * @return Identifier for the resource e.g. Patient/455435464698
    */
-  def resolveIdentifier(resourceTypeExpr:ExpressionContext, identifierValueExpr:ExpressionContext, identifierSystemExpr:ExpressionContext):Seq[FhirPathResult] = {
+  def resolveIdentifier(resourceTypeExpr: ExpressionContext, identifierValueExpr: ExpressionContext, identifierSystemExpr: Option[ExpressionContext]): Seq[FhirPathResult] = {
     val identityService = checkIdentityService()
 
     val resourceType = evaluateToSingleString(resourceTypeExpr)
     val identifierValue = evaluateToSingleString(identifierValueExpr)
-    val identifierSystem = evaluateToSingleString(identifierSystemExpr)
-    if(resourceType.isEmpty || identifierValue.isEmpty)
+    val identifierSystem = identifierSystemExpr.flatMap(evaluateToSingleString)
+    if (resourceType.isEmpty || identifierValue.isEmpty)
       throw FhirPathException(s"Invalid function call resolveIdentifier. The 'identifierValue' and 'resourceType' parameters are mandatory!")
 
     try {
@@ -32,21 +34,22 @@ class FhirPathIdentityServiceFunctions(context:FhirPathEnvironment) extends Abst
         .map(matchingId => FhirPathString(s"${resourceType.get}/$matchingId"))
         .toSeq
     } catch {
-      case t:Throwable => throw FhirPathException("Problem while calling identity service!", t)
+      case t: Throwable => throw FhirPathException("Problem while calling identity service!", t)
     }
   }
 
   /**
    * Check if terminology service is supplied
+   *
    * @return
    */
-  private def checkIdentityService():IFhirIdentityService = {
-    if(context.identityService.isEmpty)
+  private def checkIdentityService(): IFhirIdentityService = {
+    if (context.identityService.isEmpty)
       throw FhirPathException("Identity service function calls are not supported with the current configuration of onfhir FHIR Path engine!")
     context.identityService.get
   }
 
-  private def evaluateToSingleString(expr:ExpressionContext):Option[String] = {
+  private def evaluateToSingleString(expr: ExpressionContext): Option[String] = {
     // Evaluate code
     val exprResult = new FhirPathExpressionEvaluator(context, context._this).visit(expr)
     if (exprResult.length > 1 || !exprResult.forall(_.isInstanceOf[FhirPathString]))
@@ -60,7 +63,8 @@ class FhirPathIdentityServiceFunctions(context:FhirPathEnvironment) extends Abst
  * Function library factory for this library
  */
 object FhirPathIdentityServiceFunctionsFactory extends IFhirPathFunctionLibraryFactory {
-  final val defaultPrefix:String = "idxs"
+  final val defaultPrefix: String = "idxs"
+
   /**
    *
    * @param context
