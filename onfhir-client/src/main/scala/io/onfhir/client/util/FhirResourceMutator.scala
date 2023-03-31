1322d9987bae7a92728c7221855dae737b7d53ec
Tuncay NAMLI <tuncay@srdc.com.tr>
2023-03-31 17:05:58 +0300

    Change resource mutator to apply more than one patch

--- onfhir-client/src/main/scala/io/onfhir/client/util/FhirResourceMutator.scala
+++ onfhir-client/src/main/scala/io/onfhir/client/util/FhirResourceMutator.scala
@@ -2,6 +2,7 @@ package io.onfhir.client.util
 
 import io.onfhir.api.Resource
 import io.onfhir.api.util.FhirPatchUtil
+import io.onfhir.client.util.FhirResourceMutator.FhirMutableResource
 import io.onfhir.path.FhirPathEvaluator
 import org.json4s.JsonAST.JValue
 
@@ -11,7 +12,7 @@ import org.json4s.JsonAST.JValue
 object FhirResourceMutator {
   implicit val ic = scala.language.implicitConversions
 
-  class FhirMutableResource(resource:Resource) {
+  class FhirMutableResource(var resource:Resource) {
 
     /**
      * Add a FHIR element under the given path (FHIR Patch add logic)
@@ -21,10 +22,12 @@ object FhirResourceMutator {
      * @param isArray   If the element to add should be an array (needed when the element does not exist  and if true a array with given single value is created)
      * @return
      */
-    def addElement(fhirPath:String, name:String, value:JValue, isArray:Boolean = false):Resource = {
-      parsePath(fhirPath)
-        .map(parsedPath => FhirPatchUtil.applyAddPatch(resource, parsedPath, name, value, isArray))
-        .getOrElse(resource)
+    def addElement(fhirPath:String, name:String, value:JValue, isArray:Boolean = false):FhirMutableResource = {
+      resource =
+        parsePath(fhirPath)
+          .map(parsedPath => FhirPatchUtil.applyAddPatch(resource, parsedPath, name, value, isArray))
+          .getOrElse(resource)
+      this
     }
 
     /**
@@ -36,11 +39,12 @@ object FhirResourceMutator {
      * @return
     */
     @throws[IllegalArgumentException]
-    def addElementOrThrowExc(fhirPath:String, name:String, value:JValue, isArray:Boolean = false):Resource = {
-      parsePath(fhirPath) match {
+    def addElementOrThrowExc(fhirPath:String, name:String, value:JValue, isArray:Boolean = false):FhirMutableResource = {
+      resource = parsePath(fhirPath) match {
         case None => throw new IllegalArgumentException(s"Given FHIR path $fhirPath does not indicate any element for the given resource!")
         case Some(parsedPath) => FhirPatchUtil.applyAddPatch(resource, parsedPath, name, value, isArray)
       }
+      this
     }
 
     /**
@@ -50,8 +54,10 @@ object FhirResourceMutator {
      * @param isArray   If the element to add should be an array (needed when the element does not exist  and if true a array with given single value is created)
      * @return
      */
-    def addRootElement(name:String, value:JValue, isArray:Boolean = false):Resource =
-      FhirPatchUtil.applyAddPatch(resource, Nil, name, value, isArray)
+    def addRootElement(name:String, value:JValue, isArray:Boolean = false):FhirMutableResource = {
+      resource = FhirPatchUtil.applyAddPatch(resource, Nil, name, value, isArray)
+      this
+    }
 
     /**
      * Insert a FHIR element to given index
@@ -64,10 +70,12 @@ object FhirResourceMutator {
      */
     @throws[IndexOutOfBoundsException]
     @throws[IllegalArgumentException]
-    def insertElement(fhirPath:String, index:Int, value:JValue):Resource = {
-      parsePath(fhirPath)
-        .map(parsedPath => FhirPatchUtil.applyInsertPatch(resource, parsedPath, index, value))
-        .getOrElse(resource)
+    def insertElement(fhirPath:String, index:Int, value:JValue):FhirMutableResource = {
+      resource =
+        parsePath(fhirPath)
+          .map(parsedPath => FhirPatchUtil.applyInsertPatch(resource, parsedPath, index, value))
+          .getOrElse(resource)
+      this
     }
 
     /**
@@ -79,11 +87,12 @@ object FhirResourceMutator {
      */
     @throws[IndexOutOfBoundsException]
     @throws[IllegalArgumentException]
-    def insertElementOrThrowExc(fhirPath:String, index:Int, value:JValue):Resource = {
-      parsePath(fhirPath) match {
+    def insertElementOrThrowExc(fhirPath:String, index:Int, value:JValue):FhirMutableResource = {
+      resource = parsePath(fhirPath) match {
         case None => throw new IllegalArgumentException(s"Given FHIR path $fhirPath does not indicate any element for the given resource!")
         case Some(parsedPath) => FhirPatchUtil.applyInsertPatch(resource, parsedPath, index, value)
       }
+      this
     }
 
     /***
@@ -91,10 +100,12 @@ object FhirResourceMutator {
      * @param fhirPath  Fhir path to the element or item in an repetitive element
      * @return
      */
-    def deleteElement(fhirPath:String):Resource = {
-      parsePath(fhirPath)
-        .map(parsedPath => FhirPatchUtil.applyDeletePatch(resource, parsedPath))
-        .getOrElse(resource)
+    def deleteElement(fhirPath:String):FhirMutableResource = {
+      resource =
+        parsePath(fhirPath)
+          .map(parsedPath => FhirPatchUtil.applyDeletePatch(resource, parsedPath))
+          .getOrElse(resource)
+      this
     }
 
     /**
@@ -103,11 +114,12 @@ object FhirResourceMutator {
      * @return
      */
     @throws[IllegalArgumentException]
-    def deleteElementOrThrowExc(fhirPath:String):Resource = {
-      parsePath(fhirPath) match {
+    def deleteElementOrThrowExc(fhirPath:String):FhirMutableResource = {
+      resource = parsePath(fhirPath) match {
         case None => throw new IllegalArgumentException(s"Given FHIR path $fhirPath does not indicate any element for the given resource!")
         case Some(parsedPath) => FhirPatchUtil.applyDeletePatch(resource, parsedPath)
       }
+      this
     }
 
     /**
@@ -116,10 +128,13 @@ object FhirResourceMutator {
      * @param value     JSON Value to replace by
      * @return
      */
-    def replaceElement(fhirPath:String, value:JValue):Resource = {
-      parsePath(fhirPath).map(parsedPath =>
-        FhirPatchUtil.applyReplacePatch(resource, parsedPath, value)
-      ).getOrElse(resource)
+    def replaceElement(fhirPath:String, value:JValue):FhirMutableResource = {
+      resource =
+        parsePath(fhirPath)
+          .map(parsedPath =>
+            FhirPatchUtil.applyReplacePatch(resource, parsedPath, value)
+          ).getOrElse(resource)
+      this
     }
 
     /**
@@ -129,11 +144,12 @@ object FhirResourceMutator {
      * @return
      */
     @throws[IllegalArgumentException]
-    def replaceElementOrThrowExc(fhirPath:String, value:JValue):Resource = {
-      parsePath(fhirPath) match {
+    def replaceElementOrThrowExc(fhirPath:String, value:JValue):FhirMutableResource = {
+      resource = parsePath(fhirPath) match {
         case None => throw new IllegalArgumentException(s"Given FHIR path $fhirPath does not indicate any element for the given resource!")
         case Some(parsedPath) => FhirPatchUtil.applyReplacePatch(resource, parsedPath, value)
       }
+      this
     }
 
     /**
@@ -147,10 +163,11 @@ object FhirResourceMutator {
      */
     @throws[IndexOutOfBoundsException]
     @throws[IllegalArgumentException]
-    def moveElement(fhirPath:String, source:Int, destination:Int):Resource = {
-      parsePath(fhirPath).map(parsedPath =>
+    def moveElement(fhirPath:String, source:Int, destination:Int):FhirMutableResource = {
+      resource = parsePath(fhirPath).map(parsedPath =>
         FhirPatchUtil.applyMovePatch(resource, parsedPath, source, destination)
       ).getOrElse(resource)
+      this
     }
 
     /**
@@ -164,11 +181,12 @@ object FhirResourceMutator {
      */
     @throws[IndexOutOfBoundsException]
     @throws[IllegalArgumentException]
-    def moveElementOrThrowExc(fhirPath:String, source:Int, destination:Int):Resource = {
-      parsePath(fhirPath) match {
+    def moveElementOrThrowExc(fhirPath:String, source:Int, destination:Int):FhirMutableResource = {
+      resource = parsePath(fhirPath) match {
         case None => throw new IllegalArgumentException(s"Given FHIR path $fhirPath does not indicate any element for the given resource!")
         case Some(parsedPath) => FhirPatchUtil.applyMovePatch(resource, parsedPath, source, destination)
       }
+      this
     }
 
     /**
@@ -191,5 +209,7 @@ object FhirResourceMutator {
    * @return
    */
   implicit def convertToFhirMutableResource(resource: Resource):FhirMutableResource = new FhirMutableResource(resource)
+
+  implicit def convertToResource(fhirMutableResource: FhirMutableResource):Resource = fhirMutableResource.resource
 
 }
