c47fe1849004fb33ee51ce842966d45cfe5d09b2
A. Anil Sinaci <anil@srdc.com.tr>
2023-04-03 14:30:40 +0300

    :wrench: chore(docker): Fix docker compose and sample-setup configuration for testin.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,8 @@
 # Execute one of the following commands from the project.root.directory (../)
 
 docker build -f docker/Dockerfile-addJar -t srdc/onfhir:r4 .
+docker build -f docker\Dockerfile-addJar -t srdc/onfhir:r4 .
+
 docker build -f docker/Dockerfile-buildJar -t srdc/onfhir:r4 .
+docker build -f docker\Dockerfile-buildJar -t srdc/onfhir:r4 .
 
