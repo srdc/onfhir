2088aaf616d1603f43e4cf12362408da89cc861c
A. Anil Sinaci <anil@srdc.com.tr>
2024-02-08 09:55:19 +0300

    :wrench: chore: Update the build script to examplify r5 image building.

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,7 @@
 # Execute one of the following commands from the project.root.directory (../)
 
 docker build -f docker/Dockerfile-addJar-r4 -t srdc/onfhir:r4 .
+docker build -f docker/Dockerfile-addJar-r5 -t srdc/onfhir:r5 .
 docker build -f docker\Dockerfile-addJar-r4 -t srdc/onfhir:r4 .
 
 docker build -f docker/Dockerfile-buildJar-r4 -t srdc/onfhir:r4 .
