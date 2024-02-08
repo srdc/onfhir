62ce950b462f172df0f9edbc228e487ad5722f90
dogukan10 <dogukan10cav@gmail.com>
2024-02-08 10:09:16 +0300

    :wrench: build/conf: Pass FHIR server version to Dockerfile as a build argument

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
 # Execute one of the following commands from the project.root.directory (../)
 
-docker build -f docker/Dockerfile-addJar-r4 -t srdc/onfhir:r4 .
-docker build -f docker/Dockerfile-addJar-r5 -t srdc/onfhir:r5 .
-docker build -f docker\Dockerfile-addJar-r4 -t srdc/onfhir:r4 .
+docker build -f docker/Dockerfile-addJar --build-arg FHIR_VERSION=r4 -t srdc/onfhir:r4 .
+docker build -f docker/Dockerfile-addJar --build-arg FHIR_VERSION=r5 -t srdc/onfhir:r5 .
+docker build -f docker\Dockerfile-addJar --build-arg FHIR_VERSION=r4 -t srdc/onfhir:r4 .
 
-docker build -f docker/Dockerfile-buildJar-r4 -t srdc/onfhir:r4 .
-docker build -f docker\Dockerfile-buildJar-r4 -t srdc/onfhir:r4 .
+docker build -f docker/Dockerfile-buildJar --build-arg FHIR_VERSION=r4 -t srdc/onfhir:r4 .
+docker build -f docker\Dockerfile-buildJar --build-arg FHIR_VERSION=r4 -t srdc/onfhir:r4 .
 
