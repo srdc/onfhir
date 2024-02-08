33acb3f7f3bb2f0badfd5c7da8920aafb8281f09
A. Anil Sinaci <anil@srdc.com.tr>
2024-02-08 09:53:32 +0300

    :wrench: chore: Add new dockerfiles and update the build scripts to handle both r4 and r5 images.

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,8 @@
 # Execute one of the following commands from the project.root.directory (../)
 
-docker build -f docker/Dockerfile-addJar -t srdc/onfhir:r4 .
-docker build -f docker\Dockerfile-addJar -t srdc/onfhir:r4 .
+docker build -f docker/Dockerfile-addJar-r4 -t srdc/onfhir:r4 .
+docker build -f docker\Dockerfile-addJar-r4 -t srdc/onfhir:r4 .
 
-docker build -f docker/Dockerfile-buildJar -t srdc/onfhir:r4 .
-docker build -f docker\Dockerfile-buildJar -t srdc/onfhir:r4 .
+docker build -f docker/Dockerfile-buildJar-r4 -t srdc/onfhir:r4 .
+docker build -f docker\Dockerfile-buildJar-r4 -t srdc/onfhir:r4 .
 
